d908792735a722a045bf8ce3089e32439316fbef
soumaya sassi <soumaya.sassimahfoudh@esprit.tn>
2022-10-19 09:24:29 +0100

    CR Student

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\StudentType;
 use App\Repository\StudentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,5 +21,21 @@ public function index() : Response
        return $this->render("student/read.html.twig",
            ["students"=>$students]);
     }
+    #[Route('/addstudent', name: 'addc')]
+    public function  add(ManagerRegistry $doctrine, Request  $request) : Response
+    { $student = new Student() ;
+        $form = $this->createForm(StudentType::class, $student);
+        $form->add('ajouter', SubmitType::class) ;
+        $form->handleRequest($request);
+        if ($form->isSubmitted())
+        { $em = $doctrine->getManager();
+            $em->persist($student);
+            $em->flush();
+            return $this->redirectToRoute('read_student');
+        }
+        return $this->renderForm("student/add.html.twig",
+            ["f"=>$form]) ;
 
+
+    }
 }
